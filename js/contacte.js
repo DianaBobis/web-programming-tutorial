09b80b9e008ee6f7900112f47690f12a436be721
Diana <dianabobis@gmail.com>
2017-02-03 15:59:45 +0200

    Json

--- js/contacte.js
+++ js/contacte.js
@@ -1,24 +1,47 @@
 //document.getElementById('agenda').getElementsByTagName('tbody')[0].innerHTML += contact;
-
 // $('#agenda tbody').html(contact);
+
+//Varianta JS
+// function getContactsHTML(contact) {
+//     return '<tr>'+
+//         '<td>' + contact[0] + '</td>'+
+//         '<td>' + contact[1] + '</td>'+
+//         '<td>' + contact[2] + '</td>'+
+//         '</tr>';
+// };
+//
+// var contacts = [
+//     ['Diana', 'b', '12'],
+//     ['Mihai','x', '234'],
+//     ['vasile', 'y', '454']
+// ];
+// var contactsHTML = '';
+// for (var i=0; i<contacts.length; i++) {
+//    /*Afiseaza in consola */
+//    console.info(contacts[i]);
+//    contactsHTML += getContactsHTML(contacts[i]);
+// };
+//
+// $('#agenda tbody').html(contactsHTML);
+
+//varianta JSON
+
 function getContactsHTML(contact) {
     return '<tr>'+
-        '<td>' + contact[0] + '</td>'+
-        '<td>' + contact[1] + '</td>'+
-        '<td>' + contact[2] + '</td>'+
+        '<td>' + contact.firstname + '</td>'+
+        '<td>' + contact.lastname + '</td>'+
+        '<td>' + contact.phone + '</td>'+
         '</tr>';
 };
+var contactsHTML='';
 
 var contacts = [
-    ['Diana', 'b', '12'],
-    ['Mihai','x', '234'],
-    ['vasile', 'y', '454']
+    {firtsname:'Diana', lastname:'Bobis', phone : '111'},
+    {firtsname : 'Mihai', lastname :'Pop', phone : '2345'},
+    {firtsname : 'Andrei', lastname :'Mircea', phone : '543'}
 ];
-var contactsHTML = '';
-for (var i=0; i<contacts.length; i++) {
-   /*Afiseaza in consola */
-   console.info(contacts[i]);
-   contactsHTML += getContactsHTML(contacts[i]);
-};
 
-$('#agenda tbody').html(contactsHTML);
+for (var i=0; i<contacts.length; i++) {
+    console.info(contacts[i]);
+    contactsHTML += getContactsHTML(contacts[i]);
+};
