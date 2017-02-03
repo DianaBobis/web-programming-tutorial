c283c32012f86087e9b4b17b570476925a09a787
Diana <dianabobis@gmail.com>
2017-02-03 15:46:13 +0200

    new page Contacte.html adaugare contacte

---
+++ js/contacte.js
@@ -0,0 +1,25 @@
+//document.getElementById('agenda').getElementsByTagName('tbody')[0].innerHTML += contact;
+
+// $('#agenda tbody').html(contact);
+function getContactsHTML(contact) {
+    return '<tr>'+
+        '<td>' + contact[0] + '</td>'+
+        '<td>' + contact[1] + '</td>'+
+        '<td>' + contact[2] + '</td>'+
+        '</tr>';
+};
+
+var contacts = [
+    ['Diana', 'b', '12'],
+    ['Mihai','x', '234'],
+    ['vasile', 'y', '454']
+];
+var contactsHTML = '';
+for (var i=0; i<contacts.length; i++) {
+   /*Afiseaza in consola */
+   console.info(contacts[i]);
+   contactsHTML += getContactsHTML(contacts[i]);
+
+};
+
+$('#agenda tbody').html(contactsHTML);
